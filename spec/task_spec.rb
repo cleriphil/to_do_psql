8ccaf6da62d2caa9245ca2a40aa2b1a47410781b
cleri philiastides & chris le compte <student@epicodus.com>
2015-04-28 09:32:46 -0700

    Add and pass Task.all spec and Task#save spec

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -9,9 +9,17 @@ describe(Task) do
     end
   end
 
-  # describe(".all") do
-  #   it("is empty at first") do
-  #     expect(Task.all()).to(eq([]))
-  #   end
-  # end
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new("wash the lion")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
 end
