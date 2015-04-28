f71fea921633ae92391c0c18d5274de09dbaa63b
cleri philiastides & chris le compte <student@epicodus.com>
2015-04-28 09:24:40 -0700

    Add and pass Task#description spec

---
+++ spec/task_spec.rb
@@ -0,0 +1,17 @@
+require('rspec')
+require('task')
+
+describe(Task) do
+  describe("#description") do
+    it("lets you give it a description") do
+      test_task = Task.new("scrub the zebra")
+      expect(test_task.description()).to(eq("scrub the zebra"))
+    end
+  end
+
+  # describe(".all") do
+  #   it("is empty at first") do
+  #     expect(Task.all()).to(eq([]))
+  #   end
+  # end
+end
