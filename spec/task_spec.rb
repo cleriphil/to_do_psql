cd08f0350a61b6ae0196f289d2fe90d350b9d7b0
cleri philiastides & chris le compte <student@epicodus.com>
2015-04-28 09:47:07 -0700

    Add Task.clear() before all specs

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('task')
 
 describe(Task) do
+  before() do
+    Task.clear()
+  end
+
   describe("#description") do
     it("lets you give it a description") do
       test_task = Task.new("scrub the zebra")
@@ -19,7 +23,6 @@ describe(Task) do
     it("adds a task to the array of saved tasks") do
       test_task = Task.new("wash the lion")
       test_task.save()
-      # test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
