8eb605432767a1b5932b5ebeb564ae10b93e293c
cleri philiastides & ian bevel <student@epicodus.com>
2015-05-04 16:58:27 -0700

    Add list.rb and list_spec.rb

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,6 +1,6 @@
-require('rspec')
-require('pg')
-require('task')
+require("rspec")
+require("pg")
+require("task")
 
 DB = PG.connect({:dbname => 'to_do_test'})
 
@@ -11,31 +11,38 @@ RSpec.configure do |config|
 end
 
 describe(Task) do
-  describe("#description") do
-    it("lets you give it a description") do
-      test_task = Task.new({:description => "scrub the zebra"})
-      expect(test_task.description()).to(eq("scrub the zebra"))
-    end
-  end
-
   describe(".all") do
     it("is empty at first") do
       expect(Task.all()).to(eq([]))
     end
   end
-  describe('#==') do
-    it('is the same task if it has the same description') do
-      task1 = Task.new({:description => 'learn SQL'})
-      task2 = Task.new({:description => 'learn SQL'})
-      expect(task1).to(eq(task2))
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
     end
   end
 
-  describe('#save') do
-    it('adds a task to the array of saved tasks') do
-      test_task = Task.new({:description => 'learn SQL'})
-      test_task.save()
-      expect(Task.all()).to(eq([test_task]))
+  describe("#description") do
+    it("lets you read the description out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.description()).to(eq("learn SQL"))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  describe("#==") do
+    it("is the same task if it has the same description and list ID") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
     end
   end
 end
