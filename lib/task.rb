6bc8f4d3a4f8ba036222b2819fcd60ea68da64b7
cleri philiastides & ian bevel <student@epicodus.com>
2015-05-04 15:56:49 -0700

    Add database to to_do with PSQL

--- lib/task.rb
+++ lib/task.rb
@@ -1,23 +1,15 @@
 class Task
-  @@all_tasks = []
+  attr_reader(:description)
 
-  define_method(:initialize) do |description|
-    @description = description
-  end
-
-  define_method(:description) do
-    @description
+  define_method(:initialize) do |attributes|
+    @description = attributes.fetch(:description)
   end
 
   define_singleton_method(:all) do
-    @@all_tasks
+    []
   end
 
-  define_method(:save) do
-    @@all_tasks.push(self)
-  end
-
-  define_singleton_method(:clear) do
-    @@all_tasks = []
+  define_method(:==) do |another_task|
+    self.description().==(another_task.description())
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,14 +1,19 @@
 require('rspec')
+require('pg')
 require('task')
 
-describe(Task) do
-  before() do
-    Task.clear()
-  end
+DB = PG.connect({:dbname => 'to_do_test'})
 
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM task *;")
+  end
+end
+
+describe(Task) do
   describe("#description") do
     it("lets you give it a description") do
-      test_task = Task.new("scrub the zebra")
+      test_task = Task.new({:description => "scrub the zebra"})
       expect(test_task.description()).to(eq("scrub the zebra"))
     end
   end
@@ -18,20 +23,11 @@ describe(Task) do
       expect(Task.all()).to(eq([]))
     end
   end
-
-  describe("#save") do
-    it("adds a task to the array of saved tasks") do
-      test_task = Task.new("wash the lion")
-      test_task.save()
-      expect(Task.all()).to(eq([test_task]))
-    end
-  end
-
-  describe(".clear") do
-    it("empties out all the saved tasks") do
-      Task.new("wash the lion").save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
+  describe('#==') do
+    it('is the same task if it has the same description') do
+      task1 = Task.new({:description => 'learn SQL'})
+      task2 = Task.new({:description => 'learn SQL'})
+      expect(task1).to(eq(task2))
     end
   end
 end
